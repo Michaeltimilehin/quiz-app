add244c5dca3e379aec2bd031a3979930a79bba5
Michaeltimilehin <adurogboyemichael9@gmail.com>
2024-06-06 13:12:21 +0100

    add new page for the next button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+ function add(){
+  return parseInt($("#count").text()) + 1
+}
+
 $(document).ready (function (){
   $(".btn").click(function (){
   $(".show-btn").show();
@@ -10,4 +14,30 @@ $(document).ready (function (){
     $(".show-btn").hide()
     $(".back").hide()
   })
+  $(".next-btn").click(function (){
+    $(".quiz-cont").hide()
+    $(".tipsy").show()
+  })
+
+  $(".click").click(function (){
+    $(".quiz-cont").hide()
+    $(".tipsy").hide()
+    $(".gipsy").show()
+  })
+
+  $(".opta").click(function (){
+    let ans = $(this).find("span").text()
+    let number = $(".opta").index(this)
+
+    if (ans === "b" ){
+      $("#count").text(add())
+      $(".opta").eq(number).addClass("pick")
+    }else{
+      $(".opta").eq(number).addClass("pick")
+    }
+   
+    $(".opta").prop("disabled" ,true);
+  })
+
+
 }) 
